7c2d30521812038eeba74707bb25307e802933e1
josh.sorrells <jsorrells99@outlook.com>
2018-02-27 09:04:02 -0600

    created a compareTo method/intstance variables/ constructor

--- src/feb26/Fraction.java
+++ src/feb26/Fraction.java
@@ -3,5 +3,20 @@ package feb26;
 public class Fraction implements Comparable<Fraction> {
 
 
+    private int num;
+    private int denom;
 
+    public Fraction(int num, int denom){
+        this.num = denom;
+        this.denom = denom;
+        simplify();
+    }
+
+    @Override
+    public int compareTo(Fraction o) {
+        int difference = this.num*o.denom - o.num*this.denom
+                if(difference > 0)return 1;
+                else if(difference < 0)return -1;
+                return 0;
+    }
 }
