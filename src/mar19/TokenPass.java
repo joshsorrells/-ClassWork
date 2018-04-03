3efb32c6622e4f9d9df8c9e43da0a4c93290a2a3
joshsorrells <jsorrells99@outlook.com>
2018-04-03 09:13:13 -0500

    Wrote remove Variety and getNumBoxes

---
+++ src/mar19/TokenPass.java
@@ -0,0 +1,12 @@
+package mar19;
+
+public class TokenPass {
+
+    private int[] board;
+    private int currentPlayer;
+
+    public TokenPass(int playerCount){
+        board = new int[playerCount];
+        for(int i = 0; i < playerCount; i++)
+    }
+}
